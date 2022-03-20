a9ca5714f73164426a3d4321c11458debc53ac5b
FernanMarsiglia <fernanh96@hotmail.com>
2022-03-20 15:46:27 -0500

    7 commit -  Finish and FinishPage class were created, the login of the last part of the form was created to complete the registration.

--- src/main/java/com/utest/tasks/Finish.java
+++ src/main/java/com/utest/tasks/Finish.java
@@ -1,4 +1,32 @@
 package com.utest.tasks;
 
-public class Finish {
+import com.utest.userinterface.FinishPage;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Tasks;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Enter;
+
+public class Finish implements Tasks{
+    public static Finish theRegister() {
+        return Tasks.instrumented(Finish.class);
+    }
+    @Override
+    public <T extends Actor> void performAs (T actor) {
+        actor.attempsTo(
+        if((FinishPage.CONFIRM_PAGE).isDisplayed()) {
+            Enter.theValue("Guayabon123.").into(FinishPage.INPUT_PASSWORD);
+            Enter.theValue("Guayabon123.").into(FinishPage.CONFIRM_PASSWORD);
+            Click.on(FinishPage.TERMSOFUSE);
+            Click.on(FinishPage.TERMSOFPRIV);
+            Click.on(FinishPage.COMPLETESETUP_BUTTON);
+            Thread.sleep(2000);
+        }
+        else{
+            System.out.print("Register last page was not found");
+        }
+        );
+
+    }
+
 }
---
+++ src/main/java/com/utest/userinterface/FinishPage.java
@@ -0,0 +1,15 @@
+package com.utest.userinterface;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.By;
+
+public class FinishPage extends PageObjectt{
+        public static final Target CONFIRM_PAGE = Target.the("tells us if we advance to the next step correctly").located(By.xpath("//*[@class='signup-consent__text--highlight']"));
+        public static final Target INPUT_PASSWORD = Target.the("where do we write the password").located(By.id("password"));
+        public static final Target CONFIRM_PASSWORD = Target.the("where do we confirm the password").located(By.id("confirmPassword"));
+        public static final Target TERMSOFUSE = Target.the("where do we accept the terms of use").located(By.xpath("//*[@name='termOfUse']"));
+        public static final Target TERMSOFPRIV = Target.the("where do we accept the privacy and security policy").located(By.xpath("//*[@name='privacySetting']"));
+        public static final Target COMPLETESETUP_BUTTON = Target.the("button to end the registration").located(By.xpath("//*[@id='laddaBtn']"));
+
+}
