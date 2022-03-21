0cd918d8f8d152cca63d948f613a770f2bd99315
FernanMarsiglia <fernanh96@hotmail.com>
2022-03-20 20:56:41 -0500

    8 commit -  redefinition of stepdefinition class and answer class was created to confirm the register.

---
+++ src/main/java/com/utest/questions/Answer.java
@@ -0,0 +1,30 @@
+package com.utest.questions;
+
+import com.utest.userinterface.FinishPage;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+import net.serenitybdd.screenplay.question.Text;
+
+public class Answer implements Question<Boolean> {
+    private String question = "Welcome to the world's largest community of freelance software testers!";
+
+    public Answer(String question){
+        this.question = question;
+    }
+
+    public static Answer toThe(String question) {
+        return new Answer(question);
+    }
+
+    @Override
+    public Boolean answeredBy(Actor actor){
+        boolean result;
+        String nameFinishPage= Text.of(FinishPage.NAMEFINISH_PAGE).viewedBy(actor).asString();
+        if (question.equals(nameFinishPage)) {
+            result = true;
+        }else {
+            result = false;
+        }
+        return = result
+    }
+}
--- src/test/java/com/utest/stepdefinitions/utestAutomation.java
+++ src/test/java/com/utest/stepdefinitions/utestAutomation.java
@@ -1,13 +1,15 @@
 package com.utest.stepdefinitions;
 
+import com.utest.questions.Answer;
 import com.utest.tasks.Go;
 import com.utest.tasks.Login;
-import com.utest.tasks.;
-import com.utest.tasks.;
+import com.utest.tasks.Finish;
+import com.utest.tasks.Fills;
 import cucumber.api.java.en.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.GivenWhenThen;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
@@ -21,15 +23,16 @@ public class utestAutomation {
     @Given("^than Fernan wants to join in the utest platform$")
     public void thanFernanWantsToJoinInTheUtestPlatform() {
         OnStage.theActorCalled("Fernan").wasAbleTo(Go.toUtestPage(), (Login.firstPage()));
-
-    @When("^he enters to the website and fills the first register$")
-    public void heEntersToTheWebsiteAndFillsTheFirtRegister() {
-        OnStage.theActorInTheSpotlight().attemptsTo(Fills.theForm());
     }
 
-    @Then("^he fills out the last part of the registration form$")
-    public void heFillsOutTheLastPartOfTheRegistrationForm() {
-        OnStage.theActorInTheSpotlight().attemptsTo(Finish.theRegister());
-        }
+    @When("^he enters to the website and fills the register$")
+    public void heEntersToTheWebsiteAndFillsTheRegister() {
+        OnStage.theActorInTheSpotlight().attemptsTo(Fills.theForm(), (Finish.theRegister()));
     }
+
+    @Then("^he confirm the register$")
+    public void heConfirmTheRegister() {
+        OnStage.theActorInTheSpotlight().should(GivenWhenThen.seeThat(Answer.toThe(question)));
+    }
+
 }
